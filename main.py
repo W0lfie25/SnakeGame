e462e64dcc29c8487fdf37b35603276636df8742
W0lfie25 <drosinakeioanna@gmail.com>
2026-05-14 14:00:16 +0300

    Updated game and converted to EXE

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import json
 import os
 import sys
 
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 pygame.init()
 pygame.mixer.init()
 
